0c5c346a7d1232cf33460c69e4478ab1b28e31b9
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-11-28 12:29:38 +0100

    Convert proxy to list. Files reorganization.

--- scripts/proxy_scrapper/proxy_scrapper.py
+++ scripts/proxy_scrapper/proxy_scrapper.py
@@ -16,18 +16,3 @@ def scrap_proxy():
                         sep=':',
                         header=False,
                         mode='a')
-
-
-def delete_duplicates():
-    try:
-        proxy_list = pd.read_csv('proxy_list.csv', sep=':')
-        proxy_list = proxy_list.drop_duplicates()
-        proxy_list.to_csv('proxy_list.csv',
-                          encoding='utf-8',
-                          index=False,
-                          sep=':',
-                          header=False,
-                          mode='w')
-    except FileNotFoundError:
-        scrap_proxy()
-        delete_duplicates()
