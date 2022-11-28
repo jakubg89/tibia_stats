458f695df43a99f6f5bc8a8b9ab3150c17f3b419
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-11-28 12:37:52 +0100

    Header list.

---
+++ scripts/tibiacom_scrapper/headers.py
@@ -0,0 +1,5 @@
+def headers():
+    headers_list = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                                  ' AppleWebKit/537.36 (KHTML, like Gecko)'
+                                  ' Chrome/99.0.4844.74 Safari/537.36'}
+    return headers_list
