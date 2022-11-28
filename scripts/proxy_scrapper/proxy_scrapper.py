3904bc92a681677e788cafabb93166ce15940adb
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-11-28 11:51:05 +0100

    Exception FileNotFound added. Checking status code.

--- scripts/proxy_scrapper/proxy_scrapper.py
+++ scripts/proxy_scrapper/proxy_scrapper.py
@@ -6,29 +6,28 @@ def scrap_proxy():
     url = 'https://free-proxy-list.net/'
     request = requests.get(url)
 
-    # future check if website is reachable
-    # if request.status_code == 200:
-
-    proxy_df = pd.DataFrame(data=pd.read_html(request.text)[0])
-    proxy_df = proxy_df[proxy_df['Anonymity'] == 'elite proxy']
-    proxy_df = proxy_df[['IP Address', 'Port']].reset_index(drop=True).drop_duplicates(subset='IP Address')
-    proxy_df.to_csv('proxy_list.csv',
-                    encoding='utf-8',
-                    index=False,
-                    sep=':',
-                    header=False,
-                    mode='a')
+    if request.status_code == 200:
+        proxy_df = pd.DataFrame(data=pd.read_html(request.text)[0])
+        proxy_df = proxy_df[proxy_df['Anonymity'] == 'elite proxy']
+        proxy_df = proxy_df[['IP Address', 'Port']].reset_index(drop=True).drop_duplicates(subset='IP Address')
+        proxy_df.to_csv('proxy_list.csv',
+                        encoding='utf-8',
+                        index=False,
+                        sep=':',
+                        header=False,
+                        mode='a')
 
 
 def delete_duplicates():
-    proxy_list = pd.read_csv('proxy_list.csv', sep=':')
-    proxy_list = proxy_list.drop_duplicates()
-    proxy_list.to_csv('proxy_list.csv',
-                      encoding='utf-8',
-                      index=False,
-                      sep=':',
-                      header=False,
-                      mode='w')
-
-
-
+    try:
+        proxy_list = pd.read_csv('proxy_list.csv', sep=':')
+        proxy_list = proxy_list.drop_duplicates()
+        proxy_list.to_csv('proxy_list.csv',
+                          encoding='utf-8',
+                          index=False,
+                          sep=':',
+                          header=False,
+                          mode='w')
+    except FileNotFoundError:
+        scrap_proxy()
+        delete_duplicates()
