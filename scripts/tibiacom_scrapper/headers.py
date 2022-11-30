3bb166df76cefb5dc8f58be42aac9f1629b31de6
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-11-30 11:53:47 +0100

    Deleted proxy usage. Deleted unneeded code.

--- scripts/tibiacom_scrapper/headers.py
+++ scripts/tibiacom_scrapper/headers.py
@@ -1,5 +1,3 @@
 def headers():
-    headers_list = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                                  ' AppleWebKit/537.36 (KHTML, like Gecko)'
-                                  ' Chrome/99.0.4844.74 Safari/537.36'}
+    headers_list = {'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'}
     return headers_list
--- scripts/tibiacom_scrapper/highscores_scrapper.py
+++ scripts/tibiacom_scrapper/highscores_scrapper.py
@@ -5,7 +5,7 @@ from time import sleep
 import data
 import headers
 import time
-from scripts.proxy_scrapper.proxy_functions import get_proxy_list
+# from scripts.proxy_scrapper.proxy_functions import get_proxy_list
 import worlds
 
 
@@ -13,7 +13,7 @@ class ScrapHighscores:
     def __init__(self):
         category = data.category()
         self.request_headers = headers.headers()
-        self.proxy_list = get_proxy_list()
+        # self.proxy_list = get_proxy_list()
 
         self.how_many_sites = 0
         self.file_name = time.strftime('%d_%m_%Y', time.localtime())
@@ -22,12 +22,12 @@ class ScrapHighscores:
         self.block_first_row = 1
         self.text = ''
 
-        self.access = True
-        self.access2 = True
-        self.proxy_num = 0
+        self.access_count_sites = True
+        self.access_scrap_sub_site = True
+        # self.proxy_num = 0
 
         for world in worlds.world():
-            print(world, ' ', end='')
+            # print(world, ' ', end='')
             sleep(round(random.randint(5, 20)))
             for prof in data.profession():
                 self.url = 'https://www.tibia.com/community/?subtopic=highscores&' \
@@ -39,7 +39,7 @@ class ScrapHighscores:
                                                    category=category,
                                                    prof=prof,
                                                    verify=True)
-                print(prof, end='')
+                # print(prof, end='')
                 self.check = self.count_sites() + 1
                 for site_num in range(1, self.check):
                     self.url2 = 'https://www.tibia.com/community/?subtopic=highscores&' \
@@ -52,60 +52,63 @@ class ScrapHighscores:
                                                                 prof=prof,
                                                                 site_num=site_num,
                                                                 verify=True)
-
-                    self.access2 = True
-                    while self.access2:
-                        self.proxies = {
-                            "http": self.proxy_list[self.proxy_num],
-                        }
+                    # print(self.request.status_code)
+                    self.access_scrap_sub_site = True
+                    while self.access_scrap_sub_site:
+                        # self.proxies = {
+                        #    "http": self.proxy_list[self.proxy_num]
+                        # }
                         self.request = requests.get(self.url2,
                                                     headers=self.request_headers,
-                                                    proxies=self.proxies,
+                                                    # proxies=self.proxies,
                                                     verify=True)
                         self.soup = BeautifulSoup(self.request.text, 'html.parser')
 
                         if self.request.status_code == 403:
-                            self.access2 = True
-                            sleep(round(random.uniform(0.1, 0.2), 3))
-                            self.proxy_num += 1
-                            if self.proxy_num == len(self.proxy_list):
-                                self.proxy_num = 0
+                            self.access_scrap_sub_site = True
+                            sleep(round(random.uniform(10, 20), 3))
+
+                            # self.proxy_num += 1
+                            # print(self.proxy_num)
+                            # if self.proxy_num == len(self.proxy_list):
+                            #    self.proxy_num = 0
                         else:
                             self.scrap_sub_site()
 
-                            sleep(round(random.uniform(0.1, 0.2), 3))
-                            print(str(site_num) + '/' + str(self.check) + '-', end='')
-                            print('ok ', end='')
-                            self.proxy_num += 1
-                            if self.proxy_num >= len(self.proxy_list):
-                                self.proxy_num = 0
-                            print(self.proxy_list[self.proxy_num], self.proxy_num, len(self.proxy_list))
-                            self.access2 = False
-                print()
+                            sleep(round(random.uniform(0.8, 2), 3))
+                            # print(str(site_num) + '/' + str(self.check) + '-', end='')
+                            # print('ok ', end='')
+                            # self.proxy_num += 1
+                            # if self.proxy_num >= len(self.proxy_list):
+                            #     self.proxy_num = 0
+                            # print(self.proxy_list[self.proxy_num], self.proxy_num, len(self.proxy_list))
+                            self.access_scrap_sub_site = False
+                # print()
                 self.check = 0
 
     def count_sites(self):
-        self.how_many_sites = 0
-        self.access = True
-        while self.access:
-            self.proxies = {
-                "http": self.proxy_list[self.proxy_num],
-            }
+        # self.how_many_sites = 0
+        self.access_count_sites = True
+        while self.access_count_sites:
+            # self.proxies = {
+            #    "http": self.proxy_list[self.proxy_num]
+            # }
             self.request = requests.get(self.url,
                                         headers=self.request_headers,
-                                        proxies=self.proxies)
+                                        # proxies=self.proxies)
+                                        )
             self.soup = BeautifulSoup(self.request.text, 'html.parser')
 
             if self.request.status_code == 403:
-                self.access = True
-                sleep(round(random.uniform(0.25, 0.4), 3))
-                self.proxy_num += 1
+                self.access_count_sites = True
+                sleep(round(random.uniform(10, 20), 3))
+                # self.proxy_num += 1
                 # print(self.proxy_num, end='')
-                if self.proxy_num > len(self.proxy_list):
-                    self.proxy_num = 0
+                # if self.proxy_num > len(self.proxy_list):
+                #    self.proxy_num = 0
             else:
                 # print('ok')
-                self.access = False
+                self.access_count_sites = False
                 for self.i in self.soup.findAll('span', {'class': 'PageLink'}):
                     self.how_many_sites += 1
                 self.how_many_sites = int(self.how_many_sites / 2)
@@ -138,9 +141,6 @@ class ScrapHighscores:
                 self.column_count += 1
 
     def savefile(self, text):
-        with open(self.path, 'a') as self.file:
+        with open(self.path, 'a', encoding='utf-8') as self.file:
             self.file.write(text)
         self.file.close()
-
-
-ScrapHighscores()
