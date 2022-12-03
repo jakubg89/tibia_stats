ba3658bb8a90b71b4fb22b2537d4e5c7dfc15c9b
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-12-03 10:13:47 +0100

    Adding condition to check if list is not empty.

--- scripts/database/data.py
+++ scripts/database/data.py
@@ -13,34 +13,35 @@ def add_news_to_db():
 
     # get latest id list
     id_list = get_latest_news_id()
-    for i in id_list:
-        with connection.cursor() as cursor:
+    if id_list:
+        for i in id_list:
+            with connection.cursor() as cursor:
 
-            # check if database already has that id return 1 or 0
-            query = "SELECT EXISTS(SELECT id_on_tibiacom FROM news WHERE id_on_tibiacom = {id}) as truth;"\
-                .format(id=i)
-            cursor.execute(query)
-            exist = cursor.fetchone()
+                # check if database already has that id return 1 or 0
+                query = "SELECT EXISTS(SELECT id_on_tibiacom FROM news WHERE id_on_tibiacom = {id}) as truth;" \
+                    .format(id=i)
+                cursor.execute(query)
+                exist = cursor.fetchone()
 
-            # check the result and perform insert if its not in database
-            if exist[0] != 1:
-                single_news = dataapi.get_specific_news(i)
+                # check the result and perform insert if its not in database
+                if exist[0] != 1:
+                    single_news = dataapi.get_specific_news(i)
 
-                # prepare data
-                date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                content = single_news['content'].replace("'", "\\'")
-                content_html = single_news['content_html'].replace("'", "\\'")
+                    # prepare data
+                    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    content = single_news['content'].replace("'", "\\'")
+                    content_html = single_news['content_html'].replace("'", "\\'")
 
-                # execute query
-                cursor.execute("INSERT INTO News (news_id,"
-                               " id_on_tibiacom, url_tibiacom, type, content, content_html, date_added)"
-                               " VALUES (NULL, {id}, '{url}', '{type}', '{content}', '{content_html}', '{date}');"
-                               .format(id=single_news['id'],
-                                       url=single_news['url'],
-                                       type=single_news['type'],
-                                       content=content,
-                                       content_html=content_html,
-                                       date=date))
+                    # execute query
+                    cursor.execute("INSERT INTO News (news_id,"
+                                   " id_on_tibiacom, url_tibiacom, type, content, content_html, date_added)"
+                                   " VALUES (NULL, {id}, '{url}', '{type}', '{content}', '{content_html}', '{date}');"
+                                   .format(id=single_news['id'],
+                                           url=single_news['url'],
+                                           type=single_news['type'],
+                                           content=content,
+                                           content_html=content_html,
+                                           date=date))
 
 
 # compare list of latest 90 days history of news from tibia.com to existing ones in database
