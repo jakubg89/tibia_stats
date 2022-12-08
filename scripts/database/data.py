b6f3ef7401981be239274067bff35fc1c57a5964
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-12-08 06:11:15 +0100

    Added loading settings from django

--- scripts/database/data.py
+++ scripts/database/data.py
@@ -1,11 +1,17 @@
 from django.db import connection
-from os import environ
+from tibia_stats.wsgi import *
+# from os import environ
+import os
 import pandas as pd
 import scripts.tibiadata_API.get_data as dataapi
 from datetime import datetime
 from bs4 import BeautifulSoup
+import json
+from pathlib import Path
+from main.models import World
 
-environ.setdefault('DJANGO_SETTINGS_MODULE', 'tibia_stats.settings')
+
+# environ.setdefault('DJANGO_SETTINGS_MODULE', 'tibia_stats.settings')
 
 
 def add_backslashes(text):
@@ -23,7 +29,8 @@ def format_content(raw_html):
 
         # check if there are all attributes
         try:
-            images_in_news[i]['style'] = f"width: {images_in_news[i]['width']}px; height: {images_in_news[i]['height']}px;"
+            images_in_news[i][
+                'style'] = f"width: {images_in_news[i]['width']}px; height: {images_in_news[i]['height']}px;"
         except KeyError as x:
             if x == 'height':
                 images_in_news[i]['style'] = f"width: {images_in_news[i]['width']}px;"
@@ -45,7 +52,6 @@ def format_content(raw_html):
 
 # adding news to database if it not exists
 def add_news_ticker_to_db():
-
     # get latest id list
     id_list = get_latest_news_ticker_id()
 
@@ -95,7 +101,7 @@ def get_latest_news_ticker_id():
         all_id_tibia_com.append(row['id'])
 
     # getting list of id from db which are not older than 90 days
-    with connection.cursor()as cursor:
+    with connection.cursor() as cursor:
         cursor.execute(
             'SELECT id_on_tibiacom FROM news WHERE date_added >= (NOW() - INTERVAL 90 DAY) AND (type="ticker");'
         )
@@ -106,6 +112,7 @@ def get_latest_news_ticker_id():
     # return list of unique id that are not included in db
     return list(set(all_id_tibia_com) - set(all_id_db))
 
+
 # # # # # # # News ticker end # # # # # # #
 
 
@@ -113,7 +120,6 @@ def get_latest_news_ticker_id():
 
 # adding news to database if it not exists
 def add_news_to_db():
-
     # get latest id list
     id_list = get_latest_news_id()
 
@@ -166,7 +172,7 @@ def get_latest_news_id():
         all_id_tibia_com.append(row['id'])
 
     # getting list of id from db which are not older than 90 days
-    with connection.cursor()as cursor:
+    with connection.cursor() as cursor:
         cursor.execute(
             'SELECT id_on_tibiacom FROM news WHERE date_added >= (NOW() - INTERVAL 90 DAY) AND (type="news");'
         )
@@ -176,6 +182,7 @@ def get_latest_news_id():
 
     # return list of unique id that are not included in db
     return list(set(all_id_tibia_com) - set(all_id_db))
+
 
 # # # # # # # News end # # # # # # #
 
@@ -191,7 +198,6 @@ def add_boss_to_db():
     if boss_info:
         date = datetime.now().strftime("%Y-%m-%d")
         with connection.cursor() as cursor:
-
             # execute query
             cursor.execute(
                 f"INSERT INTO Boosted (boosted_id, name, image_url, type, date) "
@@ -208,11 +214,72 @@ def add_creature_to_db():
     if creature_info:
         date = datetime.now().strftime("%Y-%m-%d")
         with connection.cursor() as cursor:
-
             # execute query
             cursor.execute(
                 f"INSERT INTO Boosted (boosted_id, name, image_url, type, date) "
                 f"VALUES (NULL, '{creature_info['name']}', '{creature_info['image_url']}', '{category}', '{date}');"
             )
 
+
 # # # # # # # Boosted creature/boss end # # # # # # #
+
+
+# # # # # # # Worlds # # # # # # #
+
+def add_worlds_information_to_db():
+    worlds_information = dataapi.get_worlds_information()
+    path_to_json = Path(__file__).resolve().parent.parent
+    values = json.load(open(os.path.join(path_to_json, 'tibiacom_scrapper\\temp\\data_value\\data.json')))
+
+    for world in worlds_information:
+
+        # check if list is not empty, if yes we just skip that part. All news tickers are up-to-date.
+        if world:
+            with connection.cursor() as cursor:
+
+                # check if database already has that id return 1 or 0
+                cursor.execute(
+                    f"SELECT EXISTS (SELECT name FROM world WHERE name = '{world['name']}') as truth;"
+                )
+                exist = cursor.fetchone()
+
+                # check the result and perform insert if it's not in database
+                if exist[0] != 1:
+
+                    # getting data of creation date if world is initially protected
+                    if world['battleye_date'] == 'release':
+                        creation_date = dataapi.get_world_details(world['name'])
+                        date = creation_date['creation_date'] + '-01'
+                        battleye_value = values['beprotection']['Initially Protected']
+                    elif world['battleye_date'] == '':
+                        creation_date = dataapi.get_world_details(world['name'])
+                        date = creation_date['creation_date'] + '-01'
+                        battleye_value = values['beprotection']['Unprotected']
+                    else:
+                        date = world['battleye_date']
+                        battleye_value = values['beprotection']['Protected']
+
+                    # add value based on location
+                    if world['location'] == 'Europe':
+                        location_value = 0
+                    elif world['location'] == 'South America':
+                        location_value = 1
+                    else:
+                        location_value = 2
+
+                    # execute query
+                    cursor.execute(
+                        f"INSERT INTO world (world_id, name, name_value, pvp_type, pvp_type_value, battleye_protected, battleye_date, battleye_value, location, location_value) VALUES (NULL, "
+                        f"'{world['name']}', "
+                        f"'{values['world'][world['name']]}', "  # name_value
+                        f"'{world['pvp_type']}', "  # pvp_type
+                        f"'{values['pvp_type'][world['pvp_type']]}', "  # pvp_value
+                        f"'{world['battleye_protected']}', "  # battleye_protected
+                        f"'{date}',"  # battleye_date
+                        f"'{battleye_value}',"  # battleye_value
+                        f"'{world['location']}',"  # location
+                        f"'{location_value}');"  # location_value
+                    )
+
+# # # # # # # Worlds end # # # # # # #
+
