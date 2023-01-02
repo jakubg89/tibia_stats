a6295e64407fa47e44c3baefb919336a956d816e
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-01-02 18:08:53 +0100

    World transfers custom tags for displaying dict value and tibia coins amount added

--- main/templatetags/custom_tags.py
+++ main/templatetags/custom_tags.py
@@ -13,3 +13,13 @@ def put_space(text):
 @register.filter(name='format_date')
 def format_date(date):
     return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+
+
+@register.filter(name='get_value')
+def get_value(my_dict, key):
+    return my_dict[key]
+
+
+@register.filter(name='tc_amount')
+def get_value(transfers):
+    return transfers * 750
