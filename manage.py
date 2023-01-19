498f285f0c9a6aa8df733d2f4229b550c9fa9fe1
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-01-19 12:49:10 +0100

    Pointing env file

--- manage.py
+++ manage.py
@@ -2,10 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
 
 def main():
     """Run administrative tasks."""
+
+    # dotenv settings
+    dotenv.load_dotenv(
+        os.path.join(os.path.dirname(__file__), '.env')
+    )
+
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tibia_stats.settings')
     try:
         from django.core.management import execute_from_command_line
