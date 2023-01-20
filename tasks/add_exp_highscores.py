efaf47c6e22a7c87077a31e735d3045b4cbf2bf0
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-01-20 11:39:40 +0100

    Bug fix highscores bulk crashing wrong date format

--- tasks/add_exp_highscores.py
+++ tasks/add_exp_highscores.py
@@ -4,8 +4,7 @@ import django
 if __name__ == '__main__':
     import sys
     sys.path.insert(0, '/django-projects/tibia-stats')
-    from scripts.database.data import filter_highscores_data
-
+    from scripts.database.add_data import filter_highscores_data
 
 os.environ["DJANGO_SETTINGS_MODULE"] = 'tibia_stats.settings'
 django.setup()
--- tasks/add_players_online.py
+++ tasks/add_players_online.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     import sys
     sys.path.insert(0, '/django-projects/tibia-stats')
-    from scripts.database.data import add_world_online_history
+    from scripts.database.add_data import add_world_online_history
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "tibia_stats.settings"
 django.setup()
