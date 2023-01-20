244b52bf3e7d7b6feb8595515210ec6926d0f1a3
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-01-20 13:51:04 +0100

    Added functions to copy only needed records to table and delete old ones.

--- tasks/add_exp_highscores.py
+++ tasks/add_exp_highscores.py
@@ -4,10 +4,13 @@ import django
 if __name__ == '__main__':
     import sys
     sys.path.insert(0, '/django-projects/tibia-stats')
-    from scripts.database.add_data import filter_highscores_data
+    from scripts.database.add_data import (filter_highscores_data,
+                                           move_only_active_players,
+                                           delete_old_highscores_date)
 
 os.environ["DJANGO_SETTINGS_MODULE"] = 'tibia_stats.settings'
 django.setup()
 
 filter_highscores_data()
-
+move_only_active_players()
+delete_old_highscores_date()
