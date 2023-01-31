6dcb1358cf5b59f37514341946b146a1d8b518d8
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-01-31 13:56:20 +0100

    Task add_exp_highscores expanded. Checking status of steps in highscore task. Added ability to re-run script from last finished part. Removing files from temp directory.

--- tasks/add_exp_highscores.py
+++ tasks/add_exp_highscores.py
@@ -1,16 +1,128 @@
+if __name__ == '__main__':
+    import sys
+
+    sys.path.insert(0, '/django-projects/tibia-stats')
+    from scripts.database.add_data import (scrap_charms,
+                                           scrap_experience,
+                                           prepare_data_and_db,
+                                           insert_name_change,
+                                           insert_world_changes,
+                                           insert_highscores)
+from tibia_stats.wsgi import *
+from main.models import Tasks
+from datetime import datetime, timedelta
+import datetime
+import logging
+import glob
 import os
 import django
 
-if __name__ == '__main__':
-    import sys
-    sys.path.insert(0, '/django-projects/tibia-stats')
-    from scripts.database.add_data import (filter_highscores_data,
-                                           move_only_active_players,
-                                           delete_old_highscores_date)
 
 os.environ["DJANGO_SETTINGS_MODULE"] = 'tibia_stats.settings'
 django.setup()
 
-filter_highscores_data()
-move_only_active_players()
-delete_old_highscores_date()
+# Task status:
+# queued
+# done
+
+
+def main():
+    now = datetime.datetime.now()
+    date = now - timedelta(days=1, hours=2)
+    if start_time():
+        # date = '2023-01-29 17:00:00'
+        logging.info(f'{datetime.datetime.now()} TASK START')
+        delete_tasks()
+        logging.info(f'{datetime.datetime.now()} Tasks deleted.')
+        # delete_files()
+        logging.info(f'{datetime.datetime.now()} Files deleted.')
+        create_tasks(date)
+        logging.info(f'{datetime.datetime.now()} Tasks created.')
+        perform_script()
+
+    else:
+        perform_script()
+
+
+def perform_script():
+    logging.info(f'{datetime.datetime.now()} Perform script started.')
+
+    list_and_date = get_tasks_to_list()
+    if list_and_date:
+        to_do = list_and_date['to_do']
+        date = list_and_date['date']
+        # date = '2023-01-29 17:00:00'
+        for i in to_do:
+            logging.info(f'Performing {i} - {datetime.datetime.now()}')
+            eval(i + '(date)')
+    else:
+        logging.info(f'{datetime.datetime.now()} All tasks are done. EXIT')
+        exit()
+
+
+def delete_tasks():
+    clear_data_query = Tasks.objects.all()
+    clear_data_query._raw_delete(clear_data_query.db)
+
+
+def delete_files():
+    for file in glob.glob("/django-projects/tibia-stats/temp/"):
+        os.remove(file)
+
+
+def start_time():
+    start = datetime.time(3, 45, 0)
+    end = datetime.time(4, 15, 0)
+    current = datetime.datetime.now().time()
+    return start <= current <= end
+
+
+def create_tasks(date):
+    tasks = {}
+    status = "queued"
+    tasks_list = [
+        "scrap_experience",
+        "scrap_charms",
+        #"prepare_data_and_db",
+        #"insert_name_change",
+        #"insert_world_changes",
+        #"insert_highscores"
+    ]
+
+    for idx, i in enumerate(tasks_list):
+        tasks.update({idx: {
+            "task_name": i,
+            "status": status,
+            "date": date
+        }})
+    tasks_for_insert = []
+    for task in tasks:
+        task_obj = Tasks(
+            task_name=tasks[task]["task_name"],
+            status=tasks[task]["status"],
+            date=tasks[task]["date"]
+        )
+        tasks_for_insert.append(task_obj)
+    Tasks.objects.bulk_create(tasks_for_insert)
+    logging.info('Tasks created.')
+
+
+def get_tasks_to_list():
+    get_tasks = Tasks.objects.all().values('task_name', 'status', 'date').order_by('task')
+
+    if get_tasks:
+        to_do = []
+        for i in get_tasks:
+            if (i['status'] != 'done') and (i['task_name'] != "checked_characters"):
+                to_do.append(i["task_name"])
+        list_and_date = {'date': get_tasks[0]['date'],
+                         'to_do': to_do}
+        return list_and_date
+    else:
+        logging.info(f'{datetime.datetime.now()} All tasks are done. EXIT')
+        exit()
+
+
+if __name__ == "__main__":
+    main()
+
