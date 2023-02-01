29b7ae6b0eadb906dbf5cbc244077b50913c9bc1
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-02-01 13:58:07 +0100

    Logging msg changed.

--- tasks/task_checker.py
+++ tasks/task_checker.py
@@ -8,6 +8,32 @@ from main.models import Tasks
 import logging
 import django
 import datetime
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="/django-projects/tibia-stats/logs/task_checker.log",
+    filemode="a",
+)
+
+# All of this is already happening by default!
+sentry_logging = LoggingIntegration(
+    level=logging.INFO,  # Capture info and above as breadcrumbs
+    event_level=logging.ERROR,  # Send errors as events
+)
+
+sentry_sdk.init(
+    dsn=os.environ.get("DSN"),
+    integrations=[
+        sentry_logging,
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
 
 
 def main():
@@ -25,18 +51,18 @@ def check_running_tasks():
         for current_task in running_tasks:
             name = current_task[-1].split("/")
             if name[-1] == "add_exp_highscores.py":
-                logging.info(f'TASK CHECKER - {datetime.datetime.now()} - '
+                logging.info(f'ADD HIGHSCORES - {datetime.datetime.now()} - '
                              f'add_exp_highscores.py running')
                 exit()
 
     task_status = Tasks.objects.all().values('status')
     for task in task_status:
         if task['status'] != 'done':
-            logging.info(f'TASK CHECKER - {datetime.datetime.now()} - Executing file.')
+            logging.info(f'ADD HIGHSCORES - {datetime.datetime.now()} - Executing file.')
             exec(open('/django-projects/tibia-stats/tasks/python add_exp_highscores.py').read())
             exit()
 
-    logging.info(f'TASK CHECKER - {datetime.datetime.now()} - All tasks are done.')
+    logging.info(f'ADD HIGHSCORES - {datetime.datetime.now()} - All tasks are done.')
     exit()
 
 
