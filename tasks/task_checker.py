bce08e86202b35b7e55e192d1917777f4528fe78
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-02-01 12:10:14 +0100

    Adjusted checker.

--- tasks/task_checker.py
+++ tasks/task_checker.py
@@ -1,8 +1,12 @@
 if __name__ == '__main__':
     import sys
-
     sys.path.insert(0, '/django-projects/tibia-stats')
+
 import psutil
+from tibia_stats.wsgi import *
+from main.models import Tasks
+import logging
+import django
 
 
 def main():
@@ -15,16 +19,18 @@ def check_running_tasks():
         if psutil.Process(i).name() == 'python':
             running_tasks.append(psutil.Process(i).cmdline())
 
-    '''running_tasks = [['/django-projects/tibia-stats/venv-tibia-stats/bin/python',
-                      '/django-projects/tibia-stats/tasks/add_exp_highscores.py'],
-                     ['python', 'a.py']]'''
-    for current_task in running_tasks:
-        name = current_task[-1].split("/")
-        if name[-1] == "add_exp_highscores.py":
+    if running_tasks:
+        for current_task in running_tasks:
+            name = current_task[-1].split("/")
+            if name[-1] == "add_exp_highscores.py":
+                exit()
+
+    task_status = Tasks.objects.all().values('status')
+    for task in task_status:
+        if task['status'] != 'done':
+            exec(open('/django-projects/tibia-stats/tasks/python add_exp_highscores.py').read())
             exit()
-        else:
-            pass
-            # wywolanie pliku
+    exit()
 
 
 if __name__ == "__main__":
